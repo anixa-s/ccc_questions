94800d13784a163624a45afeabde59414a84cd4e
anixa-s <anikasingh715@gmail.com>
2025-02-01 21:15:43 -0500

    edited code

--- CCC'05_J1.py
+++ CCC'05_J1.py
@@ -4,18 +4,28 @@ evening = int(input())
 weekend = int(input())
 
 # Calculating for Plan A
-cost_minutes_A = daytime - 100 
-day_cost_A = cost_minutes_A * 0.25 
-evening_cost_A = evening * 0.15 
-weekend_cost_A = weekend * 0.2 
-total_A = day_cost_A + evening_cost_A + weekend_cost_A
-
+if daytime > 100:
+    cost_minutes_A = daytime - 100 
+    day_cost_A = cost_minutes_A * 0.25 
+    evening_cost_A = evening * 0.15 
+    weekend_cost_A = weekend * 0.2 
+    total_A = day_cost_A + evening_cost_A + weekend_cost_A
+else:
+    total_A = 0 
+    
 # Calculating for Plan B
-cost_minutes_B = daytime - 250 
-day_cost_B = cost_minutes_B * 0.45 
-evening_cost_B = evening * 0.35 
-weekend_cost_B = weekend * 0.25 
-total_B = day_cost_B + evening_cost_B + weekend_cost_B
+if daytime > 250:
+    cost_minutes_B = daytime - 250 
+    day_cost_B = cost_minutes_B * 0.45 
+    evening_cost_B = evening * 0.35 
+    weekend_cost_B = weekend * 0.25 
+    total_B = day_cost_B + evening_cost_B + weekend_cost_B
+else:
+    total_B = 0 
+    
+# Output 
+print(f"Plan A costs {round(total_A, 2)}")
+print(f"Plan B costs {round(total_B, 2)}")
 
 # Comparing Plans A & B
 if total_A > total_B: 
@@ -24,7 +34,3 @@ elif total_A == total_B:
     print("Plan A and B are the same price")
 else:
     print("Plan A is cheapest")
-
-# Output 
-print(f"Plan A costs {round(total_A, 2)}")
-print(f"Plan B costs {round(total_B, 2)}")
