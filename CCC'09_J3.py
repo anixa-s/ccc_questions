2b88228b1c3fd7979ea2ed042ebdf3f02279085c
anixa-s <anikasingh715@gmail.com>
2025-02-09 22:52:30 -0500

    added instructions

---
+++ CCC'09_J3.py
@@ -0,0 +1,35 @@
+"""
+A mobile cell service provider in Ottawa broadcasts an automated time standard to its mobile users that reflects 
+the local time at the user's actual location in Canada. This ensures that text messages have a 
+valid local time attached to them.
+
+For example, when it is 1420 in Ottawa on Tuesday February 24, 2009 (specified using military, 24 hour format), 
+the times across the country are shown in the table below:
+
+Pacific Time	Mountain Time	Central Time	Eastern Time	Atlantic Time	Newfoundland Time
+Victoria, BC
+Tuesday
+2/24/2009
+1120 PST	Edmonton, AB
+Tuesday
+2/24/2009
+1220 MST	Winnipeg, MB
+Tuesday
+2/24/2009
+1320 CST	Toronto, ON
+Tuesday
+2/24/2009
+1420 EST	Halifax, NS
+Tuesday
+2/24/2009
+1520 AST	St. John's, NL
+Tuesday
+2/24/2009
+1550 Newfoundland ST
+Write a program that accepts the time in Ottawa in 24 hour format and outputs the local time in each of the cities 
+listed above including Ottawa. You should assume that the input time will be valid 
+(i.e., an integer between 0 and 2359 with the last two digits being between 00 and 59).
+
+You should note that 2359 is one minute to midnight, midnight is 0, and 13 minutes after midnight is 13. You do not need to print leading zeros, 
+and input will not contain any extra leading zeros.
+"""
